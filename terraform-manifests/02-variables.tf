33688e8af8642ebdf330e63434b1c7c823f44b98
CapstoneProjectItc <140419054+ItcTrainee2023@users.noreply.github.com>
2023-09-06 05:03:11 +0100

    First Second Commit

--- terraform-manifests/02-variables.tf
+++ terraform-manifests/02-variables.tf
@@ -7,14 +7,14 @@
 variable "location" {
   type = string
   description = "Azure Region where all these resources will be provisioned"
-  default = "Central US"
+  default = "North Europe"
 }
 
 # Azure Resource Group Name
 variable "resource_group_name" {
   type = string
   description = "This variable defines the Resource Group"
-  default = "terraform-aks"
+  default = "AKS-Forge-Niaz"
 }
 
 # Azure AKS Environment Name
@@ -43,7 +43,7 @@ variable "windows_admin_username" {
 # Windows Admin Password for k8s worker nodes
 variable "windows_admin_password" {
   type = string
-  default = "P@ssw0rd1234"
+  default = "P@ssw0rd12346789"
   description = "This variable defines the Windows admin password k8s Worker nodes"  
 }
 
